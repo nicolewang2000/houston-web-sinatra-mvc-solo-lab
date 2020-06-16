86e6ea1c7b3e998df41f555b20726c6dd1fc1378
nicolewang2000 <nicole.wang@yale.edu>
2020-06-16 14:51:39 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,40 @@ class ApplicationController < Sinatra::Base
         erb :welcome
       end
 
+      get '/index' do
+        @puppies = Puppy.all
+        erb :index
+      end 
+
+      get '/index/new' do 
+        erb :new
+      end
+
+      get '/index/:id' do
+        @puppy = Puppy.find(params[:id])
+        erb :show
+      end
+
+      post '/index' do
+        puppy = Puppy.create(params)
+        redirect "/index/#{puppy.id}"
+      end
+
+      get '/index/:id/edit' do
+        @puppy = Puppy.find(params[:id])
+        erb :edit
+      end
+
+      patch '/index/:id' do
+        puppy = Puppy.find(params[:id])
+        puppy.update(name: params[:name], breed:params[:breed])
+      redirect "/index/#{puppy.id}"
+      end
    
+      delete '/index/:id' do 
+        puppy = Puppy.find(params[:id])
+        puppy.destroy
+        redirect "/index"
+      end
+
 end
